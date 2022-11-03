89751c163cf44f33985ddc9eb4ba914cb9597021
mukto222 <116914054+mukto222@users.noreply.github.com>
2022-11-03 06:35:57 +0300

    Update update.py

--- update.py
+++ update.py
@@ -37,12 +37,12 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/mukto222/reflct-mirror"
+    UPSTREAM_REPO = "https://github.com/mukto222/Reflection-Mirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'h-code'
+    UPSTREAM_BRANCH = 'update'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
